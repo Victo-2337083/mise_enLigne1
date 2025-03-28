bbfcd950b239cbb3a351b095739eda01d04eb935
Badyp Fouowa <badyp@example.com>
2025-03-28 12:20:13 -0400

    nouvelle mise a jour

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv';
+// Dans votre fichier de démarrage
+import cors from 'cors';
+
+
+// À ajuster selon votre structure
+import routes from './src/routes/pokemon.route.js'; 
+// Créer une application express
+const app = express();
+dotenv.config();
+
+
+// Déclaration des middlewares
+app.use(cors());
+// Importer les middlewares
+app.use(express.json());
+
+
+// Démarrer le serveur
+const PORT = process.env.PORT || 3000;
+const host = process.env.HOSTNAME||'localhost';
+
+//app.use('/', routes);
+// Changer la route de base pour /api/pokemons/
+app.use('/api/pokemons/', routes);
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur ${host}:${PORT}`);
+});
+
+
+
+
+
+
